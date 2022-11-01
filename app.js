b782696ed6f08964cbda31eda2d3da76e2ab5334
rozh bahman tahir <r.bahman98@gmail.com>
2022-11-01 16:16:16 +0300

    Add basic funtionalities

--- app.js
+++ app.js
@@ -1,37 +1,98 @@
 const booksContainer = document.querySelector('.book-list');
+const form = document.querySelector('.form');
 
-const booksArray = [
-  {
-    title: 'In search of lost times',
-    author: 'Marcel Proust',
-  },
-  {
-    title: 'Ulysses',
-    author: 'James Joyce',
-  },
-];
+let booksArray = [];
 
-const createMarkup = (data) => {
+if (localStorage.getItem('books')) {
+  booksArray = JSON.parse(localStorage.getItem('books'));
+}
+
+const createBookMarkup = (data) => {
   const markUp = `
-      <div class="book-list">
-        <div class="book-one">
+
+        <li class="book-one">
           <h3 class="book-title">${data.title}</h3>
           <p class="book-author">${data.author}</p>
-          <button id="removeButton" type="button">Remove</button>
+          <button class="remove-button" type="button" data-id="${data.id}">Remove</button>
           <hr />
-        </div>
-      </div>
+        </li>
+     
   `;
+
   return markUp;
 };
 
-const injectMarkup = () => {
+const injectMarkup = (passedArr) => {
   let books = '';
 
-  booksArray.forEach((obj) => {
-    books += createMarkup(obj);
+  passedArr.forEach((obj) => {
+    books += createBookMarkup(obj);
   });
-  booksContainer?.insertAdjacentHTML('afterbegin', books);
+
+  booksContainer.innerHTML = books;
 };
 
-injectMarkup();
+injectMarkup(booksArray);
+
+function appendBook(data) {
+  const bookMarkup = `<li class="book-one">
+  <h3 class="book-title">${data.title}</h3>
+  <p class="book-author">${data.author}</p>
+  <button class="remove-button" type="button" data-id="${data.id}" >Remove</button>
+  <hr />
+</li>`;
+
+  booksContainer.insertAdjacentHTML('beforeend', bookMarkup);
+}
+
+function getDataAppend() {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const title = e.target.title.value;
+    const author = e.target.author.value;
+
+    if (!title || !author) return;
+
+    const id =
+      new Date().getTime().toString() + `${Math.trunc(Math.random() * 100)}`;
+
+    const bookData = {
+      id,
+      title,
+      author,
+    };
+
+    // Append book
+    appendBook(bookData);
+
+    // Adding to existing array
+    booksArray.push(bookData);
+
+    localStorage.setItem('books', JSON.stringify(booksArray));
+  });
+}
+
+getDataAppend();
+
+function removeBook() {
+  booksContainer.addEventListener('click', (e) => {
+    const clickedBtn = e.target.closest('.remove-button');
+
+    if (!clickedBtn) return;
+
+    console.log(clickedBtn);
+    const id = clickedBtn.dataset.id;
+
+    // Filter out clicked book
+    booksArray = booksArray.filter((book) => book.id !== id);
+
+    // Override the existing list with new list to update UI
+    injectMarkup(booksArray);
+
+    // Update local storage
+    localStorage.setItem('books', JSON.stringify(booksArray));
+  });
+}
+
+removeBook();
