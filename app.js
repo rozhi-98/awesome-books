4fa16a669e9185303ff3d22f9349d3235a666a10
rozh bahman tahir <r.bahman98@gmail.com>
2022-11-01 12:27:25 +0300

    Create array of objects and simple UI

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log('rozh');
+const booksContainer = document.querySelector('.book-list');
+
+const booksArray = [
+  {
+    title: 'In search of lost times',
+    author: 'Marcel Proust',
+  },
+  {
+    title: 'Ulysses',
+    author: 'James Joyce',
+  },
+];
+
+const createMarkup = (data) => {
+  const markUp = `
+      <div class="book-list">
+        <div class="book-one">
+          <h3 class="book-title">${data.title}</h3>
+          <p class="book-author">${data.author}</p>
+          <button id="removeButton" type="button">Remove</button>
+          <hr />
+        </div>
+      </div>
+  `;
+  return markUp;
+};
+
+const injectMarkup = () => {
+  let books = '';
+
+  booksArray.forEach((obj) => {
+    books += createMarkup(obj);
+  });
+  booksContainer?.insertAdjacentHTML('afterbegin', books);
+};
+
+injectMarkup();
